4807aa7d63a5e385c2246b6c713c11ad833eaa2f
Juan Garcia <juangalbea@gmail.com>
2019-06-26 20:51:56 +0200

    done

--- index.js
+++ index.js
@@ -10,3 +10,33 @@ mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
     console.error('Error connecting to mongo', err);
   });
 
+  
+  Recipe.deleteMany()
+  .then(()=> {
+    console.log("delete")
+    Recipe.create({
+      title: "Pizza",
+      level:'Easy Peasy',
+      ingredients: ["Sugar", "Flour", "tomatoes", "Mozarella"],
+      cuisine: "Italian",
+      dishType: "Dish",
+      image: 'https://www.recetasdesbieta.com/wp-content/uploads/2018/09/Como-hacer-pizza-casera-rapida-con-masa-de-pizza-sin-repos-1-860x380.jpg',
+      duration: 160,
+      creator: "Gennaro Contaldo"
+    })
+    .then(value => {
+      console.log(value);
+      Recipe.insertMany(data)
+      .then(value => {
+        console.log(value)
+        Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+        .then(value => {
+          console.log(value)
+          Recipe.deleteOne({title: "Carrot Cake"})
+          .then(data => {
+            console.log(data)
+          })
+      })
+    })
+  })
+  }) 
